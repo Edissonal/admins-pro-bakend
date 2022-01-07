72b40949cb1d894e0f14111a53d9297baed091c2
Edisson Alonso <edissonalonso@gmail.com>
2022-01-07 18:13:38 -0500

    rutas

--- index.js
+++ index.js
@@ -38,9 +38,10 @@ app.use('/api/upload',require('./routes/uploads'));
 
 app.get('*',(req,res) =>{
 
-    res.senFile(path.resolve(__dirname,'public/index.html' ));
+    res.sendFile(path.resolve(__dirname,'public/index.html' ));
 
 });
+
 app.listen(process.env.PORT, ()=>{
 
     console.log('Servidor corriendo en puerto'+process.env.PORT);
